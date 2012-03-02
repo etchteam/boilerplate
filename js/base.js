e9aaf447b66a925f920dec5b6fff3845dd8d1e59
Craig Dennis <craigmdennis@gmail.com>
2012-03-02 12:22:17 +0000

    Initial commit

---
+++ js/base.js
@@ -0,0 +1,39 @@
+﻿(function($) {
+	/**
+	 * Base Javascript 
+	 *
+	 * This contains javascript that will be used across your entire application
+	 * and should be used for initialisation and global event handling
+	 * 
+	 *
+	 * @author YOUR NAME <YOUR_EMAIL>
+	 * @copyright Red Bullet THIS_YEAR
+	 */
+
+	/**
+	 * Init - Pre events
+	 */
+	
+	// put code here
+
+	/**
+	 * Global Events 
+	 */
+	
+	$(document).ready( function() { 
+
+	});
+	
+	$(window).load( function() {
+		
+	});
+	
+	$(window).resize( function() {
+		
+	});
+
+	$(window).scroll( function() {
+		
+	});
+	
+})(jQuery);
